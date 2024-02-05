ad1e1cf69fd020633f463d08a545d45c1ae8ae0a
PastaLaPate <al.bekaert@gmail.com>
2024-02-05 21:47:11 +0100

    V1.2

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from moviepy.video.VideoClip import TextClip
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.video.fx.speedx import *
+from moviepy.video.fx.resize import *
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 from scipy.io.wavfile import write as write_wav
@@ -18,7 +19,6 @@ from bark.api import semantic_to_waveform
 from bark import generate_audio, SAMPLE_RATE
 import g4f
 import nltk  # we'll use this to split into sentences
-import numpy as np
 
 
 def selectMode():
@@ -36,7 +36,7 @@ def generateSubtitles(content, audiosPath):
     clips = []
     for sentence in sentences:
         audioClip = AudioFileClip(audiosPath + f"/audio-{i}.wav")
-        textClip = TextClip(txt=sentence,method="caption", stroke_color="black", fontsize=35, stroke_width=1, color="white")
+        textClip = TextClip(txt=sentence, size=(600, 1800), method="caption", kerning=2, interline=1, stroke_color="black", fontsize=50, font="Arial-Bold", stroke_width=1, color="white")
         textClip = textClip.set_duration(audioClip.duration)
         textClip = textClip.set_start(start)
         start += audioClip.duration
@@ -64,6 +64,7 @@ def getBackground(content, audiosPath):
     gameplay = randomGameplay()
     gameplay = VideoFileClip(gameplay)
     start = random.randint(0, int(gameplay.duration - duration))
+    gameplay = resize(gameplay, newsize=(1080, 1920))
     gameplay = gameplay.subclip(start, start + duration)
     gameplay = gameplay.set_duration(duration)
     return gameplay
@@ -86,41 +87,77 @@ def generateAudio(content, path, speaker="v2/en_speaker_0"):
     audio_array = semantic_to_waveform(semantic_tokens, history_prompt=speaker)
     write_wav(path, SAMPLE_RATE, audio_array)
 
+def getMusic(content, audiosPath):
+    duration = 0
+    sentences = nltk.sent_tokenize(content)
+    i = 0
+    for sentence in sentences:
+        audioClip = AudioFileClip(audiosPath + f"/audio-{i}.wav")
+        duration += audioClip.duration
+        audioClip.close()
+        i += 1
+    gameplay = AudioFileClip("./musics/music.mp3")
+    start = random.randint(0, int(gameplay.duration - duration))
+    gameplay = gameplay.subclip(start, start + duration)
+    gameplay = gameplay.set_duration(duration)
+    return gameplay
+
+def render(title):
+    f = open(f'{title}/script.txt', "r")
+    content = f.read()
+    content = content.replace("\n", " ").replace("*", " ").strip()
+    audiosPath = f'./{title}/audios'
+    videoPath = f'./{title}/video/export.mp4'
+    os.makedirs(audiosPath, exist_ok=True)
+    sentences = nltk.sent_tokenize(content)
+    speaker = "v2/en_speaker_0"
+    part = 0
+    for sentence in sentences:
+        if not os.path.exists(f"{audiosPath}/audio-{part}.wav"):
+            generateAudio(sentence, f"{audiosPath}/audio-{part}.wav")
+        part += 1
+    os.makedirs(f"./{title}/video", exist_ok=True)
+    video = CompositeVideoClip([
+        getBackground(content, audiosPath),
+        *generateSubtitles(content, audiosPath)
+    ]).set_audio(CompositeAudioClip(getAudios(content, audiosPath) + [getMusic(content, audiosPath)]))
+    video = speedx(video, 1)
+    video.write_videofile(videoPath, fps=30, preset="ultrafast")
+
+def script(prompt):
+    resp = g4f.ChatCompletion.create(
+        model="gpt-3.5",
+        provider=g4f.Provider.ChatBase,
+        messages=[{"role": "user", "content": f"Generate content for {prompt} here is a random number {random.randint(0, 1000)}"}],
+        stream=False
+    )
+    lines = resp.split("\n")
+    end = []
+    for line in lines:
+        if len(line) > 0 and line[0].isdigit():
+            end.append(line)
+    resp = ' '.join(end)
+    with open(f"{title}/script.txt", "w") as f:
+        f.write(resp)
+
+def auto():
+    title = f"{len(os.listdir("./"))}-facts"
+    os.makedirs(title, exist_ok=True)
+    script("5 random facts")
+    render(title)
+    auto()
 
 if __name__ == '__main__':
     print("Welcome to TiktokGenerator")
-    title = input('Title ? ')
-    os.makedirs(title, exist_ok=True)
     mode = selectMode()
     if mode == "script":
+        title = input('Title ? ')
+        os.makedirs(title, exist_ok=True)
         prompt = input('Prompt ? ')
-        resp = g4f.ChatCompletion.create(
-            model="gpt-4",
-            provider=g4f.Provider.Bing,
-            messages=[{"role": "user", "content": f"Generate content for {prompt}"}],
-            stream=False
-        )
-        print(resp)
-        with open(f"{title}/script.txt", "w") as f:
-            f.write(resp)
+        script(prompt)
     elif mode == "render":
-        f = open(f'{title}/script.txt', "r")
-        content = f.read()
-        content = content.replace("\n", " ").replace("*", " ").strip()
-        audiosPath = f'./{title}/audios'
-        videoPath = f'./{title}/video/export.mp4'
-        os.makedirs(audiosPath, exist_ok=True)
-        sentences = nltk.sent_tokenize(content)
-        speaker = "v2/en_speaker_0"
-        part = 0
-        for sentence in sentences:
-            if not os.path.exists(f"{audiosPath}/audio-{part}.wav"):
-                generateAudio(sentence, f"{audiosPath}/audio-{part}.wav")
-            part += 1
-        os.makedirs(f"./{title}/video", exist_ok=True)
-        video = CompositeVideoClip([
-            getBackground(content, audiosPath),
-            *generateSubtitles(content, audiosPath)
-        ]).set_audio(CompositeAudioClip(getAudios(content, audiosPath)))
-        video = speedx(video, 1.25)
-        video.write_videofile(videoPath, fps=30, preset="fast")
+        title = input('Title ? ')
+        os.makedirs(title, exist_ok=True)
+        render(title)
+    elif mode == "auto":
+        auto()
